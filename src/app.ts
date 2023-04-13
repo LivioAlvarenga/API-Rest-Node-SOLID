975ca2bbda53f1f0e57601bc28ab58404fa74ae6
Livio Alvarenga <livioalvarenga@gmail.com>
2023-04-13 06:16:11 -0300

    created users model and first ERD to test Prisma

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,11 @@
 import fastify from 'fastify'
-// import { prisma } from './lib/prisma'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+prisma.user.create({
+  data: {
+    name: 'Livio',
+    email: 'livioalvarenga@gmail.com',
+  },
+})
