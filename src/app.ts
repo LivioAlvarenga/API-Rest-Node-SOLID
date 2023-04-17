53b24dbab28d438d274064270a8fd35c314d7fcd
Livio Alvarenga <livioalvarenga@gmail.com>
2023-04-17 12:57:52 -0300

    Add logs in prisma client  in development mode

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,25 @@
 import fastify from 'fastify'
+import { z } from 'zod'
 import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-prisma.user.create({
-  data: {
-    name: 'Livio',
-    email: 'livioalvarenga@gmail.com',
-  },
+app.post('/users', async (request, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
+
+  const { name, email, password } = registerBodySchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
 })
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,6 @@
+import { env } from '@/env'
 import { PrismaClient } from '@prisma/client'
 
 export const prisma = new PrismaClient({
-  log: ['query'],
+  log: env.NODE_ENV === 'dev' ? ['query'] : [],
 })
