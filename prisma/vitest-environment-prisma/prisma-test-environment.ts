d1c94a92e45b88a1b6dfe4cbab4cecc0aee67cc0
Livio Alvarenga <livioalvarenga@gmail.com>
2023-04-20 15:22:48 -0300

    add a context isolate to e2e test

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,42 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
+
+// We need to create a fake postgresql database for each test
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please set the DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('setup') // execute before each test
+    // execute before each test
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL // set the database url for the test
+    execSync('npx prisma migrate deploy') // create the database in fake postgresql
 
     return {
-      teardown() {
-        console.log('teardown') // execute after each test
+      async teardown() {
+        // execute after each test
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        ) // drop the database in fake postgresql after the test
+
+        await prisma.$disconnect() // disconnect from the fake postgresql
       },
     }
   },
